2dcce00aafceb1d02801a89d2d9e8adae2d12006
kant300 <p.o.i.u.0987552@gmail.com>
2026-02-24 15:27:29 +0900

    Initial commit

---
+++ src/test/java/com/example/minishooping/MinishoopingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minishooping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinishoopingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
